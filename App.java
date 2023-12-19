e452822e61ddbd7399614e3788c25ffc3e8989cc
danielbirnbaum98 <daniel.birnbaum@post.runi.ac.il>
2023-12-19 16:39:36 +0200

    Create App.java

---
+++ App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String name = "Daniel";
+        System.out.println("Hello, " + name + "!");
+         
+    }
+}
